c06cec4e834936bb697d5e02728e6c1ca3024a95
Guillermo Aviles <gjaviles7@gmail.com>
2023-09-19 17:13:12 -0400

    added HomePage

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function HomePage() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
+}
+
+export default HomePage;
