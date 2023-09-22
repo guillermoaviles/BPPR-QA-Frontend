fea3a1591e79a0ea4b69467efef8e661b5ebfc05
Guillermo Aviles <gjaviles7@gmail.com>
2023-09-22 14:40:57 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import { NextUIProvider } from "@nextui-org/react";
 
 function App() {
   return (
     <div className="text-center">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <NextUIProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </NextUIProvider>
     </div>
   );
 }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,9 +3,13 @@ import axios from "axios";
 import { useEffect } from "react";
 import AddProfiles from "../components/AddProfiles";
 import ExportProfiles from "../components/ExportProfiles";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link} from "@nextui-org/react";
+import {MailIcon} from '../assets/MailIcon';
+import {LockIcon} from '../assets/LockIcon';
 
 function HomePage() {
   const [profiles, setProfiles] = useState([]);
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
   useEffect(() => {
     const handleGetProfile = async () => {
@@ -28,6 +32,60 @@ function HomePage() {
     <div className="p-4">
       <h1 className="text-3xl font-bold underline mb-4">Hello world!</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <Button onPress={onOpen} color="primary">Open Modal</Button>
+      <Modal 
+        isOpen={isOpen} 
+        onOpenChange={onOpenChange}
+        placement="top-center"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+              <ModalBody>
+                <Input
+                  autoFocus
+                  endContent={
+                    <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  }
+                  label="Email"
+                  placeholder="Enter your email"
+                  variant="bordered"
+                />
+                <Input
+                  endContent={
+                    <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  }
+                  label="Password"
+                  placeholder="Enter your password"
+                  type="password"
+                  variant="bordered"
+                />
+                <div className="flex py-2 px-1 justify-between">
+                  <Checkbox
+                    classNames={{
+                      label: "text-small",
+                    }}
+                  >
+                    Remember me
+                  </Checkbox>
+                  <Link color="primary" href="#" size="sm">
+                    Forgot password?
+                  </Link>
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="flat" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Sign in
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
         {profiles?.map((profile) => {
           return (
             <div key={profile.id} className="bg-white rounded-lg p-4 shadow-md text-left">
