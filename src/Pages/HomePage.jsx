ccf6622dd7b1edcf9c0c8f96ed8a35ffb2b714e6
Guillermo Aviles <gjaviles7@gmail.com>
2023-09-22 17:14:45 -0400

    card spacing

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -23,7 +23,7 @@ import {
 } from "@nextui-org/react";
 import { MailIcon } from "../assets/MailIcon";
 import { LockIcon } from "../assets/LockIcon";
-import avatar from "../assets/avatar.png"
+import avatar from "../assets/avatar.png";
 
 function HomePage() {
   const [profiles, setProfiles] = useState([]);
@@ -48,128 +48,114 @@ function HomePage() {
 
   return (
     <div className="p-4">
-      <h1 className="text-3xl font-bold underline mb-4">Hello world!</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <h1 className="text-xl font-bold underline mb-4">Hello world!</h1>
+      <div className="card-container">
         {profiles?.map((profile) => {
           return (
-            <Card key={profile.id} className="max-w-[500px]">
-              <CardHeader className="flex gap-3">
-                <Image
-                  alt="nextui logo"
-                  height={40}
-                  radius="sm"
-                  src={avatar}
-                  width={40}
-                />
-                <div className="flex flex-col">
-                  <p className="text-md">{profile.id}</p>
-                  {profile.isActive ? <p className="text-small text-default-500">Active: 🟢</p> : <p className="text-small text-default-500">Inactive: 🔴</p>}
-                  
-                </div>
-              </CardHeader>
-              <Divider />
-              <CardBody>
-                <div className="space-y-2">
-                  <div className="flex space-x-4 text-small items-center">
-                    <div className="text-gray-700 font-bold">
-                      Profile User Id:
+              <Card key={profile.id} className="card">
+                <CardHeader className="flex gap-3">
+                  <Image
+                    alt="nextui logo"
+                    height={40}
+                    radius="sm"
+                    src={avatar}
+                    width={40}
+                  />
+                  <div className="flex flex-col">
+                    <p className="text-md">{profile.id}</p>
+                    {profile.isActive ? (
+                      <p className="text-small text-default-500">Active: 🟢</p>
+                    ) : (
+                      <p className="text-small text-default-500">
+                        Inactive: 🔴
+                      </p>
+                    )}
+                  </div>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                    <div className="flex space-x-4 text-small items-center">
+                      <div className="text-gray-700 font-bold">
+                        Profile User Id:
+                      </div>
+                      <div>{" " + profile.profileUserId}</div>
                     </div>
-                    <div>
-                     {profile.profileUserId}
+                    <div className="flex space-x-4 text-small items-center">
+                      <div className="text-gray-700 font-bold">Username:</div>
+                      <div>{profile.username}</div>
                     </div>
-                  </div>
-                  <div className="flex space-x-4 text-small items-center">
-                    <div className="text-gray-700 font-bold">Username:</div>
-                    <div>{profile.username}</div>
-                  </div>
-                  <div className="flex space-x-4 text-small items-center">
-                    <div className="text-gray-700 font-bold">Account Type:</div>
-                    <div>{profile.accountType}</div>
-                  </div>
-                </div>
-              </CardBody>
-              <Divider />
-              <CardFooter>
-                <Link isExternal href="" className="mr-2">
-                  Edit
-                </Link>
-                <Link isExternal href="">
-                  Delete
-                </Link>
-              </CardFooter>
-            </Card>
-
-            // <div key={profile.id} className="bg-white rounded-lg p-4 shadow-md text-left">
-            //   <div className="grid grid-cols-2 gap-2">
-            //     <div className="text-gray-700 font-bold">Profile User Id:</div>
-            //     <div>{profile.profileUserId}</div>
-            //     <div className="text-gray-700 font-bold">Username:</div>
-            //     <div>{profile.username}</div>
-            //     <div className="text-gray-700 font-bold">Account Type:</div>
-            //     <div>{profile.accountType}</div>
-            //   </div>
-            // </div>
+                    <div className="flex space-x-4 text-small items-center">
+                      <div className="text-gray-700 font-bold">
+                        Account Type:
+                      </div>
+                      <div>{profile.accountType}</div>
+                    </div>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                  <Link isExternal href="" className="mr-2">
+                    Edit
+                  </Link>
+                  <Link isExternal href="">
+                    Delete
+                  </Link>
+                </CardFooter>
+              </Card>
           );
         })}
       </div>
       <Button onPress={onOpen} color="primary">
-          Open Modal
-        </Button>
-        <Modal
-          isOpen={isOpen}
-          onOpenChange={onOpenChange}
-          placement="top-center"
-        >
-          <ModalContent>
-            {(onClose) => (
-              <>
-                <ModalHeader className="flex flex-col gap-1">
-                  Log in
-                </ModalHeader>
-                <ModalBody>
-                  <Input
-                    autoFocus
-                    endContent={
-                      <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                    }
-                    label="Email"
-                    placeholder="Enter your email"
-                    variant="bordered"
-                  />
-                  <Input
-                    endContent={
-                      <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                    }
-                    label="Password"
-                    placeholder="Enter your password"
-                    type="password"
-                    variant="bordered"
-                  />
-                  <div className="flex py-2 px-1 justify-between">
-                    <Checkbox
-                      classNames={{
-                        label: "text-small",
-                      }}
-                    >
-                      Remember me
-                    </Checkbox>
-                    <Link color="primary" href="#" size="sm">
-                      Forgot password?
-                    </Link>
-                  </div>
-                </ModalBody>
-                <ModalFooter>
-                  <Button color="danger" variant="flat" onPress={onClose}>
-                    Close
-                  </Button>
-                  <Button color="primary" onPress={onClose}>
-                    Sign in
-                  </Button>
-                </ModalFooter>
-              </>
-            )}
-          </ModalContent>
-        </Modal>
+        Open Modal
+      </Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+              <ModalBody>
+                <Input
+                  autoFocus
+                  endContent={
+                    <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  }
+                  label="Email"
+                  placeholder="Enter your email"
+                  variant="bordered"
+                />
+                <Input
+                  endContent={
+                    <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  }
+                  label="Password"
+                  placeholder="Enter your password"
+                  type="password"
+                  variant="bordered"
+                />
+                <div className="flex py-2 px-1 justify-between">
+                  <Checkbox
+                    classNames={{
+                      label: "text-small",
+                    }}
+                  >
+                    Remember me
+                  </Checkbox>
+                  <Link color="primary" href="#" size="sm">
+                    Forgot password?
+                  </Link>
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="flat" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Sign in
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
       <AddProfiles />
       <ExportProfiles />
     </div>
