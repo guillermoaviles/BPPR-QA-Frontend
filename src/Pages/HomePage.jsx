3dd67242f1107ac2ee967d547eee4d35f071d078
Guillermo Aviles <gjaviles7@gmail.com>
2023-09-22 18:42:57 -0400

    button changes

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -93,70 +93,17 @@ function HomePage() {
                 </CardBody>
                 <Divider />
                 <CardFooter>
-                  <Link isExternal href="" className="mr-2">
+                  <Button color="primary">
                     Edit
-                  </Link>
-                  <Link isExternal href="">
+                  </Button>
+                  <Button color="warning" className="text-white left-3 bg-danger cursor-pointer">
                     Delete
-                  </Link>
+                  </Button>
                 </CardFooter>
               </Card>
           );
         })}
       </div>
-      <Button onPress={onOpen} color="primary">
-        Open Modal
-      </Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
-
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
-              <ModalBody>
-                <Input
-                  autoFocus
-                  endContent={
-                    <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                  }
-                  label="Email"
-                  placeholder="Enter your email"
-                  variant="bordered"
-                />
-                <Input
-                  endContent={
-                    <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                  }
-                  label="Password"
-                  placeholder="Enter your password"
-                  type="password"
-                  variant="bordered"
-                />
-                <div className="flex py-2 px-1 justify-between">
-                  <Checkbox
-                    classNames={{
-                      label: "text-small",
-                    }}
-                  >
-                    Remember me
-                  </Checkbox>
-                  <Link color="primary" href="#" size="sm">
-                    Forgot password?
-                  </Link>
-                </div>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="flat" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Sign in
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
       <AddProfiles />
       <ExportProfiles />
     </div>
