bd7c790e1312b3484a6245a2569146dc64e7e752
Guillermo Aviles <gjaviles7@gmail.com>
2023-09-22 13:48:17 -0400

    updated card ui

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -30,46 +30,26 @@ function HomePage() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {profiles?.map((profile) => {
           return (
-            <div
-              key={profile.id}
-              className="bg-white rounded-lg p-4 shadow-md"
-            >
-            <p>{profile.environment}</p>
-            <p>{profile.intendedUse}</p>
-            <p>{profile.inUse}</p>
-            <p>{profile.profileUserId}</p>
-            <p>{profile.username}</p>
-            <p>{profile.pass}</p>
-            <p>{profile.email}</p>
-            <p>{profile.firstName}</p>
-            <p>{profile.lastName}</p>
-            <p>{profile.maidenName}</p>
-            <p>{profile.birthdate}</p>
-            <p>{profile.accountType}</p>
-            <p>{profile.accountSubType}</p>
-            <p>{profile.accountNumber}</p>
-            <p>{profile.accountNickname}</p>
-            <p>{profile.accountBalance}</p>
-            <p>{profile.personalInformationEmail}</p>
-            <p>{profile.personalInformationPhone}</p>
-            <p>{profile.personalInformationAddress}</p>
-            <p>{profile.personalInformationPassword}</p>
-            <p>{profile.personalInformationQuestions}</p>
-            <p>{profile.paymentMakePayments}</p>
-            <p>{profile.cancelFutureTransfer}</p>
-            <p>{profile.makeFuturePayment}</p>
-            <p>{profile.deleteFuturePayment}</p>
-            <p>{profile.editFuturePayment}</p>
-            <p>{profile.onOffService}</p>
-            <p>{profile.addPayee}</p>
-            <p>{profile.nickname}</p>
-            <p>{profile.payeeName}</p>
-            <p>{profile.payeeAccountNumber}</p>
-            <p>{profile.ebill}</p>
+            <div key={profile.id} className="bg-white rounded-lg p-4 shadow-md">
+              <div>
+                <span className="text-gray-700 font-bold">
+                  Profile User Id:
+                </span>
+                <p>{profile.profileUserId}</p>
+              </div>
+              <div>
+                <span className="text-gray-700 font-bold">Username:</span>
+                <p>{profile.username}</p>
+              </div>
+              <div>
+                <span className="text-gray-700 font-bold">Account Type:</span>
+                <p>{profile.accountType}</p>
+              </div>
             </div>
           );
         })}
       </div>
+
       <AddProfiles />
       <ExportProfiles />
     </div>
