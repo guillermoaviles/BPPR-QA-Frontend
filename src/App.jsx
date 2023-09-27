288c411836ae33818416adc5599177d92b1540c9
merianni <merianni09@gmail.com>
2023-09-27 10:03:12 -0400

    added navbar to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Search from "./pages/Search";
-
+import NavbarComp from "./components/NavbarComp";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
 
     <div className="text-center">
+      <NavbarComp />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/profile/:id" element={<Profile />} />
       </Routes>
 
     </div>
